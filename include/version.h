6fbc9da0fea7f71e7d5b0ed898fb33755cb7ee4e
keithr101 <64606656+keithr101@users.noreply.github.com>
2025-02-14 13:28:30 -0500

    update methodoly on VERSION_DATE to fix use of __DATE__ vs __FILE__ oops

--- include/version.h
+++ include/version.h
@@ -1,8 +1,9 @@
 #ifndef VERSION_H
 #define VERSION_H
 //version
-#define VERSION_DATE "12:20 03-4-2025"
-#define BUILD_DATE __FILE__
+#define BUILD_DATE __DATE__
+//#define VERSION_DATE "12:20 03-4-2025" //Old method
+#define VERSION_DATE BUILD_DATE
 // For now make this the same as VERSION_DATE
 #define VERSION VERSION_DATE //Should be a dotted number major.minor.build
 #endif
