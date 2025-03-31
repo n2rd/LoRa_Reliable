d96e0e6b387a0286c2a9a1e22c485daaf195ab33
keithr101 <64606656+keithr101@users.noreply.github.com>
2025-03-31 16:32:52 -0400

    classif'ied the ina219 sensor code

--- include/inaSensor.h
+++ include/inaSensor.h
@@ -1,12 +1,20 @@
 #ifndef inaSensor_h
 #define inaSensor_h
+#include <Adafruit_INA219.h>
 
-#ifdef HAS_INA219
+class INASensor {
+    public:
+        void setup();
+        void outputData(Print& printDev); 
+        bool isPresent();
+        float getBusVoltage_V();
+        float getCurrent_mA(); 
+    private:
+        bool bIsPresent;
+        Adafruit_INA219 ina219;
+};
 
-//#include <Adafruit_INA3221.h>
+extern INASensor INA219;
 
-void ina_setup();
-void ina_measure();
-
-#endif //HAS_INA219
+//#endif //HAS_INA219
 #endif
--- src/inaSensor.cpp
+++ src/inaSensor.cpp
@@ -1,39 +1,44 @@
-#include "myConfig.h"
-#ifdef HAS_INA219
 #include "main.h"
-#include <Wire.h>
-#include <Adafruit_INA219.h>
 
 #include "inaSensor.h"
 
-// Create an INA3221 object
-Adafruit_INA219 ina219;
+INASensor INA219;
 
-void ina_setup() 
+void INASensor::setup() 
 { 
-  //Wire1.begin(41U,42U,400000);
-  Wire1.begin(41U,42U);
   // Initialize the INA sensor 
   if (!ina219.begin(&Wire1)) { // can use other I2C addresses or buses
-    Serial.println("Failed to find INA sensor chip");
+    log_d("Failed to find INA sensor chip");
   } else {
-    Serial.println("INA Sensor found");
+    bIsPresent = true;
+    log_v("INA Sensor found");
+  
+    // Set the INA219 to use the 32V, 1A range instead of default 2A lower precision
+    //ina219.setCalibration_32V_2A();
+    // Set the INA219 to use the 16V, 400mA range instead of default 2A lower precision
+    //ina219.setCalibration_16V_400mA();
+    // Set the INA219 to use the 32V, 1A range instead of default 2A lower precision
+    ina219.setCalibration_32V_1A();
   }
-  // Set the INA219 to use the 32V, 1A range instead of default 2A lower precision
-  //ina219.setCalibration_32V_2A();
-  // Set the INA219 to use the 16V, 400mA range instead of default 2A lower precision
-  //ina219.setCalibration_16V_400mA();
-  // Set the INA219 to use the 32V, 1A range instead of default 2A lower precision
-  ina219.setCalibration_32V_1A();
-} //ina_setup
+} //::setup
 
-void ina_measure() {
+void INASensor::outputData(Print& printDev) {
   // Display voltage and current (in mA) just for channel 0 
- float voltage = ina219.getBusVoltage_V();
- float current = ina219.getCurrent_mA(); 
- Serial.printf("Vol %4.2fV  Cur %3.0fmA\n", voltage, current);
- ps_all.printf("Vol %4.2fV  Cur %3.0fmA\n", voltage, current);
+  float voltage = getBusVoltage_V();
+  float current = getCurrent_mA(); 
+  printDev.printf("Vol %4.2fV  Cur %3.0fmA\n", voltage, current);
 }
 
+bool INASensor::isPresent() {
+  return bIsPresent;
+}
 
-#endif //HAS_INA3221
+float INASensor::getBusVoltage_V()
+{
+  return ina219.getBusVoltage_V();
+}
+
+float INASensor::getCurrent_mA()
+{
+  return ina219.getCurrent_mA();
+}
